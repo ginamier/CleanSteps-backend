61037c3ea100a547f9bd5201ed2fdf928566aa3e
ginamier <gina.mier.ro@gmail.com>
2026-04-10 08:40:56 -0600

    modify orderSchema

--- models/order.js
+++ models/order.js
@@ -1,49 +1,49 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-        required: true,
-    },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
 
-    clientName: {
-        type: String,
-        required: true,
-    },
+  clientName: {
+    type: String,
+    required: true,
+  },
 
-    clientPhone: {
-        type: String,
-        required: true,
-    },
+  clientPhone: {
+    type: String,
+    required: true,
+  },
 
-    items: [{
-        description: String, 
-        price: Number,
-        idCalzado: String,
-    }],
-    status: {
-        type: String,
-        enum: ['recibido', 'entregado'],
-        default: 'recibido'
+  items: [
+    {
+      descripcion: String,
+      precio: Number,
+      idCalzado: String,
     },
+  ],
+  status: {
+    type: String,
+    enum: ["recibido", "entregado"],
+    default: "recibido",
+  },
 
-    totalAmount: {type: Number, required: true},
-    amountPaid: {type: Number, default: 0},
+  totalAmount: { type: Number, required: true },
+  amountPaid: { type: Number, default: 0 },
 
-    secondPayment: { 
-        type: Number, 
-        default: 0 
-    },
-    deliveryDate: { 
-        type: String,
-        required: true 
-    },
-    deliveryDateSuede: { 
-        type: String 
-    }
-
+  secondPayment: {
+    type: Number,
+    default: 0,
+  },
+  deliveryDate: {
+    type: String,
+    required: true,
+  },
+  deliveryDateSuede: {
+    type: String,
+  },
 });
 
-module.exports = mongoose.model('order', orderSchema);
+module.exports = mongoose.model("order", orderSchema);
